6481b1962ae232cd6511ae89e175b7142d0b1489
piol14 <eof812@gmail.com>
2023-03-21 12:47:45 +0100

    he cambiado cositas
    Signed-off-by: piol14 <eof812@gmail.com>

---
+++ src/tema6_ejercicio9/Tema6_ejercicio9.java
@@ -0,0 +1,42 @@
+package tema6_ejercicio9;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Tema6_ejercicio9 {
+	static final int FILAS=4;
+	 static final int COLUMNAS= 3;
+	 static void ImprimirMatriz(int mat[][]) {
+		
+		 for (int i=0;i<FILAS;i++) {
+			 for (int j=0;j<COLUMNAS;j++) {
+			 System.out.println("Fila "+i+" Columna "+j+" : "+mat[i][j]);
+			 
+			 }
+			 System.out.println();
+	 }
+	 }
+	public static void main(String[] args) {
+		
+		int multiplicacion;
+		int matriz[][]= new int[FILAS][COLUMNAS];
+		int matriz2[ ][]= new int[FILAS][COLUMNAS];
+		int suma[][]= new int [FILAS][COLUMNAS];
+		Random r= new Random();
+		Scanner s=new Scanner(System.in);
+		 for (int i=0;i<matriz.length;i++)
+		 {
+			 for (int j=0;j<matriz[i].length;j++) {
+				 matriz[i][j]=r.nextInt(10);
+				 matriz2[i][j]=s.nextInt();
+				suma[i][j]=matriz[i][j]+matriz2[i][j];
+			
+				
+			 }
+			 
+		 }
+		ImprimirMatriz(suma);
+		 //Mejor: for (int j=0;j<mat[i].length;j++)
+		
+	}
+}
